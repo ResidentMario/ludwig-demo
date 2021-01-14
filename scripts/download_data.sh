866dc8b0287e4ed9394bc86560e49c7be5bff7f3
Aleksey Bilogur <aleksey@residentmar.io>
2021-01-13 20:35:09 -0500

    Fix.

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-KAGGLE_USERNAME=residentmario KAGGLE_KEY=01626f04ad79bf758eb6b106c221d26b \
+set -e
+
+if [[ -z "$KAGGLE_USERNAME" ]]; then
+    echo "KAGGLE_USERNAME environment variable not set, exiting." && exit 1
+fi
+if [[ -z "$KAGGLE_KEY" ]]; then
+    echo "KAGGLE_KEY environment variable not set, exiting." && exit 1
+fi
+
+KAGGLE_USERNAME=$KAGGLE_USERNAME KAGGLE_KEY=$KAGGLE_KEY \
     kaggle datasets download zynicide/wine-reviews
 unzip wine-reviews.zip -d /mnt/wine-reviews
 rm wine-reviews.zip
